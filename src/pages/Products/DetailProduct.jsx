2c7c9e782bdd6f2e3f0a53474709df9b8a826348
Robby94gtr <robertogramazio94@gmail.com>
2025-05-22 00:07:14 +0200

    [UPD] Axios call done

--- src/pages/Products/DetailProduct.jsx
+++ src/pages/Products/DetailProduct.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
 const DetailProduct = () => {
 
 
-    const { id } = useParams()
-    console.log(id);
+    const { id } = useParams();
+
+    const [product, setProduct] = useState({});
+
+    getProduct(() => {
+        axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) => {
+            setProduct(resp.data)
+        })
+    })
 
     return (
         <div>Dettaglio prodotto</div>
