fc6442f739ac2bb93abb71b4f5c827e3445d5705
Robby94gtr <robertogramazio94@gmail.com>
2025-05-21 23:42:25 +0200

    [UPD] Created Card

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import MainNav from '../components/MainNav'
+import { useState } from 'react'
 
 const Products = () => {
     return (
         <>
             <MainNav />
-            <h1 className='text-center my-5'>PRODOTTI</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className='text-center my-3'>PRODOTTI</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="card">
+                            <img src="" className="card-img-top img-fluid" alt="" ></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-description">description</p>
+                                <p className="card-price">price</p>
+                                <a href="#" className="btn btn-primary">share</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
