b5461bc6c4590ea269cdee73d41fe79f76c0ce24
Robby94gtr <robertogramazio94@gmail.com>
2025-05-21 23:32:46 +0200

    [UPD] Done Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import ChiSiamo from './pages/ChiSiamo'
+import Products from './pages/Products'
 
 function App() {
 
@@ -8,7 +11,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path='/' Component={Homepage} />
+          <Route path='Chi siamo' Component={ChiSiamo} />
+          <Route path='Prodotti' Component={Products} />
         </Routes>
       </BrowserRouter>
     </>
